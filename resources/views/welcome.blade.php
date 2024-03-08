a5b26e8f4af664eefa1783618753a201ffc26761
HamzaImran54 <hamza.imran@softwarealliance.dk>
2024-03-08 17:58:42 +0500

    add : model add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <title>Solid Repository CRUD</title>
 </head>
@@ -12,15 +11,15 @@
 
     <div class="container-fluid ">
         <div class="row mt-4 ">
-           <div class="col-md-12">
-                <button class="btn btn-outline-primary">Add New User</button>
-           </div>
-           <div class="col-md-12 mt-4">
+            <div class="col-md-12">
+                <button type="button" class="btn btn-outline-primary" data-bs-dismiss="modal" data-bs-toggle="modal" aria-bs-label="Close" data-bs-target="#exampleModal">Add New Student</button>
+            </div>
+            <div class="col-md-12 mt-4">
                 <table class="table">
                     <thead>
                         <tr>
                             <th scope="col">Id</th>
-                            <th scope="col">User Name</th>
+                            <th scope="col">Student Name</th>
                             <th scope="col">Email</th>
                             {{-- <th scope="col">Product Image</th> --}}
                             <th scope="col">Action</th>
@@ -28,22 +27,42 @@
                     </thead>
                     <tbody>
                         @foreach ($list as $user)
-                             <tr>
-                                 <td>{{ $user->id }}</td>
-                                 <td>{{ $user->name }}</td>
-                                 <td>{{ $user->email }}</td>
-                                 {{-- <td>4</td> --}}
-                                 <td>
-                                    <button class="btn btn-primary">Edit</button>
-                                    <button class="btn btn-danger">Delete</button>
-                                </td>
-                             </tr>
+                        <tr>
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            {{-- <td>4</td> --}}
+                            <td>
+                                <button class="btn btn-primary" data-id="{{ $user->id }}">Edit</button>
+                                <button class="btn btn-danger" data-id="{{ $user->id }}">Delete</button>
+                            </td>
+                        </tr>
                         @endforeach
                     </tbody>
                 </table>
-           </div>
+            </div>
         </div>
     </div>
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-bs-labelledby="exampleModalLabel" data-bs-backdrop="static" data-bs-keyboard="false" aria-bs-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Student Detail</h5>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 </body>
 </html>
